46d588adf42ad158b2c526f5d2fd54245a1218d9
Aashique-Ali <aashique.ali2002@gmail.com>
2025-05-05 13:33:17 +0500

    env set

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https:/api.icreativezapp.com/api",
+  baseURL: "https://api.icreativezapp.com/api",
 })
 
 api.interceptors.request.use((config) => {
